84f479fd9ad8669087d58020200a992d19d16bb2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-29 18:27:08 -0600

    feat: Add prompts package and age validation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('project');
+import prompts from 'prompts';
+
+(async () => {
+	const response = await prompts({
+		type: 'number',
+		name: 'value',
+		message: 'How old are you?',
+		validate: (value: number) => (value < 18 ? `Nightclub is 18+ only` : true),
+	});
+
+	console.log(response); // => { value: 24 }
+})();

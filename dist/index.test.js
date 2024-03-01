479b95a437b2f664a9f35945d5add4d4e106cbe3
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-29 20:38:36 -0600

    feat: Add new files for tests and prompts functionality

---
+++ dist/index.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vitest_1 = require("vitest");
+(0, vitest_1.it)('should pass', () => {
+    (0, vitest_1.expect)(true).toBeTruthy();
+});

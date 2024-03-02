32ccf844031aec7e453b717f59cee0e63b9461c9
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-02 16:14:46 -0600

    feat: Add new packages and update dependencies

--- src/templates/typescript/src/app.ts
+++ src/templates/typescript/src/app.ts
@@ -1,7 +1,11 @@
+import path from 'path';
+import ejs from 'ejs';
+import expressLayouts from 'express-ejs-layouts';
 import express from 'express';
 import compression from 'compression';
 import hlemet from 'helmet';
 import cors from 'cors';
+
 import { api as apiRouter } from './api/api.router';
 import { errorHandler, healthCheckHandler, notFoundHandler } from './app.middleware';
 
@@ -10,9 +14,20 @@ const app = express();
 app.use(cors());
 app.use(hlemet());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(path.join(process.cwd(), 'src', 'public')), { maxAge: '24h' }));
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.set('views', path.resolve(path.join(process.cwd(), 'src', 'views', 'pages')));
+app.set('layout', path.resolve(path.join(process.cwd(), 'src', 'views', 'layouts', 'main.html')));
+app.use(expressLayouts);
 
 app.use('/api', apiRouter);
 app.get('/healthz', healthCheckHandler);
+
 app.use(notFoundHandler);
 app.use(errorHandler);
 

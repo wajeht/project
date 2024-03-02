ec5527ec0126d4591949554e0c22e629d5410b3f
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-02 15:47:32 -0600

    feat: Add TypeScript dependency to project

--- src/templates/typescript/src/app.middleware.ts
+++ src/templates/typescript/src/app.middleware.ts
@@ -1,19 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 
+export function healthCheckHandler(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): Response | void {
+	return res.status(StatusCodes.OK).json({
+		message: 'ok',
+	});
+}
+
 export function notFoundHandler(req: Request, res: Response, next: NextFunction): Response | void {
-	const isApiPrefix = req.url.match(/^\/api\/v\d\//) || req.url.match(/^\/api\//);
-
-	if (isApiPrefix) {
-		return res.error({
-			statusCode: StatusCodes.NOT_FOUND,
-			message: 'Resource not found',
-			error: 'Resource not found',
-		});
-	}
-
-	return res.status(StatusCodes.NOT_FOUND).render('not-found.html', {
-		title: 'Not found',
+	return res.status(StatusCodes.NOT_FOUND).json({
+		message: 'Resource not found',
 	});
 }
 
@@ -23,36 +23,7 @@ export function errorHandler(
 	res: Response,
 	next: NextFunction,
 ): Response | void {
-	const isDevelopment = process.env.NODE_ENV === 'development';
-	const isHttpError = err instanceof HttpError;
-	const isZodError = err instanceof ZodError;
-
-	const response = {
-		success: false,
-		statusCode: isHttpError ? err.statusCode : StatusCodes.INTERNAL_SERVER_ERROR,
-		message: isHttpError ? err.message : 'Internal server error',
-		error: isDevelopment && !isZodError ? err.stack : err.message,
-	};
-
-	if (isZodError) {
-		response.message = 'Validation error';
-		response.statusCode = StatusCodes.UNPROCESSABLE_ENTITY;
-		response.error = err.issues as any;
-	}
-
-	const isApiPrefix = req.url.match(/^\/api\/v\d\//) || req.url.match(/^\/api\//);
-
-	if (isApiPrefix) {
-		return res.error(response);
-	}
-
-	if (isZodError && !isApiPrefix) {
-		req.flash('error', (response.error as any).map((e: any) => e.message).join(' '));
-		return res.status(StatusCodes.UNPROCESSABLE_ENTITY).redirect(req.originalUrl);
-	}
-
-	return res.status(response.statusCode).render('error.html', {
-		title: 'Error',
-		error: response.error,
+	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+		message: err.message,
 	});
 }
--- src/templates/typescript/src/app.ts
+++ src/templates/typescript/src/app.ts
@@ -2,11 +2,16 @@ import express from 'express';
 import compression from 'compression';
 import hlemet from 'helmet';
 import cors from 'cors';
+import { errorHandler, healthCheckHandler, notFoundHandler } from './app.middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(hlemet());
 app.use(compression());
+
+app.get('/healthz', healthCheckHandler);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export { app };

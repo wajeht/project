202787285467822f61762447967c0d9910965c49
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-01 23:41:52 -0600

    feat: Add new dependencies and middleware functions

--- src/templates/typescript/src/app.ts
+++ src/templates/typescript/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import compression from 'compression';
+import hlemet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/healthz');
+app.use(cors());
+app.use(hlemet());
+app.use(compression());
 
 export { app };
